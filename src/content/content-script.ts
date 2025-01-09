c2f4f948792d0e7c4ac687063205defea5819673
Chris Parsons <chrisparsons1127@gmail.com>
2025-01-08 22:13:02 -0800

    Start to load player data.

--- src/content/content-script.ts
+++ src/content/content-script.ts
@@ -14,16 +14,50 @@ if (isTargetPage) {
   const buttonToday = document.createElement('button');
   buttonToday.textContent = 'Autofill Lineup for Today';
   buttonToday.className = 'btn btn-primary';
+  buttonToday.id = 'ffls-autofill-today';
   
   // Add a click event to the button (example)
-  buttonToday.addEventListener('click', (e) => {
-    alert('Button clicked!');
+  buttonToday.addEventListener('click', async (e) => {
     e.preventDefault();
+
+    // Get the page with the fantasy points so we can make some uber basic projections
+    const urlObject = new URL(currentURL);
+    const queryParams = new URLSearchParams(urlObject.search);
+    queryParams.set('statType', '0');
+    urlObject.search = queryParams.toString();
+
+    // TODO: Make this an actual players object
+    const players: string[] = [];
+
+    try {
+      const response = await fetch(urlObject.href);
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const statsPage = await response.text();
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(statsPage, 'text/html');
+      const players = doc.querySelectorAll('#body-center-main table tbody tr');
+      // Make sure we set all players to the bench to start so that we can add the players positions
+      // easier.
+      console.log({players});
+    } catch (error) {
+      console.log(error);
+    }
+
+    // Now that we have all the players, we need to figure out the optimal lineup.
+
+    // Now we have the correct lineup, let's set it in the HTML, and at this point, we should be
+    // able to let the page save as it normally would. We might need to just click the submit button.
   });
 
   // Add the button after the Save Lineup button
-  const saveLineupButton = document.querySelector('#body-center-main .form-actions')
-  if (saveLineupButton) {
+  const saveLineupButton = document.querySelector('#body-center-main .form-actions');
+  // We're checking if this already exists, if so, don't add it
+  const autofillLineupButton = document.querySelector('#ffls-autofill-today')
+  if (saveLineupButton && !autofillLineupButton) {
     saveLineupButton.appendChild(buttonToday);
   }
 }
